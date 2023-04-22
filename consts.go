7579fc998089eef6ccc9a2db9dea5e2adcb650b5
edp1096 <edp1096@naver.com>
2023-04-22 23:59:24 +0900

    again

--- consts.go
+++
@@ -1,7 +0,0 @@
-package main
-
-const (
-	weightsSearchURL               = "https://huggingface.co/search/full-text?q=ggml+7b&type=model"
-	sampleVicunaWeightsDownloadURL = "https://huggingface.co/eachadea/legacy-ggml-vicuna-7b-4bit/resolve/main/ggml-vicuna-7b-4bit.bin"
-	sampleVicunaWeightsFileName    = "ggml-vicuna-7b-4bit.bin"
-)
