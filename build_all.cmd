dc933d30e6e5d5779dcfe77e6905b1d8a0f9f3f0
edp1096 <edp1096@naver.com>
2023-05-07 21:09:14 +0900

    Add zip command to build_all

--- build_all.cmd
+++ build_all.cmd
@@ -2,13 +2,13 @@
 
 cmd /c "clean.cmd"
 cmd /c "build.cmd"
-
 move /y bin\my-llama.exe bin\my-llama_cu.exe
 
 cmd /c "clean.cmd"
 
 cmd /c "mingw32-make.exe"
-
 move /y bin\my-llama.exe bin\my-llama_cpu.exe
 
 cmd /c "mingw32-make.exe clean"
+
+tar.exe -a -c -f bin\my-llama_cu.zip bin\my-llama_cu.exe bin\llama.dll
