bca3b0a625621271e87aadda090c166e00166a5e
edp1096 <edp1096@naver.com>
2023-05-31 08:37:36 +0900

    Bump

--- subtree_update.ps1
+++ subtree_update.ps1
@@ -1,5 +1,5 @@
-# Update subtree
-git subtree pull --prefix=vendors/llama.cpp https://github.com/ggerganov/llama.cpp.git master --squash
-
 # Update submodule
 git submodule update --recursive --remote
+
+# Update subtree
+git subtree pull --prefix=vendors/llama.cpp https://github.com/ggerganov/llama.cpp.git master --squash
