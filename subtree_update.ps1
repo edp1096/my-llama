80f5a99fe984793e31caa1fab3ecd61495205f8c
edp1096 <edp1096@naver.com>
2023-06-05 14:27:36 +0900

    check subtree again

--- subtree_update.ps1
+++ subtree_update.ps1
@@ -1,3 +1,3 @@
 # Update subtree
-# git subtree pull --prefix=vendors/llama.cpp https://github.com/ggerganov/llama.cpp.git master --squash
-git subtree pull --prefix=vendors/llama.cpp https://github.com/ggerganov/llama.cpp.git tags/master-136476e --squash
+git subtree pull --prefix=vendors/llama.cpp https://github.com/ggerganov/llama.cpp.git master --squash
+# git subtree pull --prefix=vendors/llama.cpp https://github.com/ggerganov/llama.cpp.git tags/master-136476e --squash
