77040b12b67e6ba01f94dde09dff25de0d8c10c5
edp1096 <edp1096@naver.com>
2023-06-05 12:08:39 +0900

    Delete submodule

--- subtree_update.ps1
+++ subtree_update.ps1
@@ -1,5 +1,3 @@
-# Update submodule
-git submodule update --recursive --remote
-
 # Update subtree
-git subtree pull --prefix=vendors/llama.cpp https://github.com/ggerganov/llama.cpp.git master --squash
+# git subtree pull --prefix=vendors/llama.cpp https://github.com/ggerganov/llama.cpp.git master --squash
+git subtree pull --prefix=vendors/llama.cpp https://github.com/ggerganov/llama.cpp.git tags/master-136476e --squash
