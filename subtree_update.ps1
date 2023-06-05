96c548f627cd3aab0b848a2c2bbfac45ba417422
edp1096 <edp1096@naver.com>
2023-06-05 16:18:10 +0900

    script

--- subtree_update.ps1
+++ subtree_update.ps1
@@ -1,3 +1,4 @@
+# git subtree add --prefix=vendors/llama.cpp https://github.com/ggerganov/llama.cpp.git master --squash
 # Update subtree
 git subtree pull --prefix=vendors/llama.cpp https://github.com/ggerganov/llama.cpp.git master --squash
 # git subtree pull --prefix=vendors/llama.cpp https://github.com/ggerganov/llama.cpp.git tags/master-136476e --squash
